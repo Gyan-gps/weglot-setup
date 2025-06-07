b44fa74cbf0dd28cc30d6b5a267be8d5215d9545
Gyan Prakash Singh <gyangps1998@gmail.com>
2025-06-07 17:25:39 +0530

    Remove GTranslate script and wrapper from _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,16 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.gtranslateSettings = {"default_language":"en","languages":["en","fr","it","es","ja","ko"],"wrapper_selector":".gtranslate_wrapper"}`
-          }}
-        />
-        <script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
-      </Head>
+      <Head />
       <body className="antialiased">
-        {/* <div className="gtranslate_wrapper"></div> */}
         <Main />
         <NextScript />
       </body>
